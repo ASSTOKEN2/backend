260102ec92e48f35e675f14eab407d83d9b13571
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-04-27 14:49:06 +0100

    sec

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/myapp').then(() => {
 
 
 var corsOptions = {
-    origin: 'https://pablo-codes.github.io/client.github.io/',
+    origin: 'https://pablo-codes.github.io',
     credentials: true,
     optionSuccessStatus: 200
 };
