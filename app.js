d3eadb698ea757897bfd3ff36034204b5ccb3b76
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-06-10 23:28:36 +0100

    node

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const { editUser, index } = require("./IndexController")
 
 dotenv.config({ path: path.join(__dirname, './config.env') })
 
-// mongoose.connect('mongodb://127.0.0.1:27017/myapp').then(() => {
-//     console.log("DB Connected Successfully")
-// }).catch((err) => {
-//     console.log("Mongo not enabled locally")
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/myapp').then(() => {
     console.log("DB Connected Successfully")
 }).catch((err) => {
-    console.log("No internet connection")
-    console.log(err)
+    console.log("Mongo not enabled locally")
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("DB Connected Successfully")
+    }).catch((err) => {
+        console.log("No internet connection")
+        console.log(err)
+    })
 })
-// })
 
 
 var corsOptions = {
